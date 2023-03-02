26eed8c7a63ba54c104faae9de88855a129de445
Jace Inglis <62110248+JaceInglis@users.noreply.github.com>
2023-03-01 16:05:34 -0800

    Import remove icon and add it to the task

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { CiCircleRemove } from 'react-icons/ci';
 
 function Todo({ tasks }) {
 
   return tasks?.map(task => (
+    <>
     <div key={task.id}>{task.task}</div>
+    <div>
+      <CiCircleRemove />
+    </div>
+    </>
   ))
 }
 
