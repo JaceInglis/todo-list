f3d891277f69798c7e0b730038928393f9834695
Jace Inglis <62110248+JaceInglis@users.noreply.github.com>
2023-03-06 20:53:42 -0800

    Add styles to todo component

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,16 +1,17 @@
 import React from 'react'
+import './Todo.css';
 
 import { CiCircleRemove } from 'react-icons/ci';
 
 function Todo({ tasks, removeTask }) {
 
   return tasks?.map((task) => (
-    <>
-    <div key={task.id}>{task.task}</div>
-    <div>
-      <CiCircleRemove onClick={() => removeTask(task.id)}/>
+    <div className='todo-wrapper'>
+      <div key={task.id}>{task.task}</div>
+      <div>
+        <CiCircleRemove onClick={() => removeTask(task.id)}/>
+      </div>
     </div>
-    </>
   ))
 }
 
