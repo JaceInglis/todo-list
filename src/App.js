6fb245528e570420629b6e6e245ec863a4db35c3
Jace Inglis <62110248+JaceInglis@users.noreply.github.com>
2023-03-07 21:43:52 -0800

    Add app styles

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ const App = () => {
 
   return (
     <div className="App">
-      <header>
-        <h1>Todo List</h1>
-        <h2>List Title</h2>
-      </header>
-      
-      <TodoList />
+      <div className='list-wrapper'>
+        <header>
+          <h1>Todo List</h1>
+          <h2>List Title</h2>
+        </header>
+        <TodoList />
+      </div>
     </div>
   );
 }
