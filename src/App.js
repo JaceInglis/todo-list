648df38c8e996feaf68ef3a8464b73ba96cc5a47
Jace Inglis <62110248+JaceInglis@users.noreply.github.com>
2023-03-08 20:15:44 -0800

    Remove uneeded div and change background

--- src/App.js
+++ src/App.js
@@ -8,9 +8,7 @@ const App = () => {
     <div className="App">
       <div className='list-wrapper'>
         <h1 className='title'>Todo List</h1>
-        <div className='todos'>
           <TodoList />
-        </div>
       </div>
     </div>
   );
