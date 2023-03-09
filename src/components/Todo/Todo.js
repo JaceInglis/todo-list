53ae8cb945a92fd7c07d6428f7fdca2e87dbf956
Jace Inglis <62110248+JaceInglis@users.noreply.github.com>
2023-03-08 20:12:34 -0800

    Add remove button transition and change color

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -8,7 +8,7 @@ function Todo({ tasks, removeTask }) {
   return tasks?.map((task) => (
     <div className='todo-wrapper'>
       <div key={task.id}>{task.task}</div>
-        <CiCircleRemove onClick={() => removeTask(task.id)}/>
+        <CiCircleRemove className='remove-button' onClick={() => removeTask(task.id)}/>
     </div>
   ))
 }
