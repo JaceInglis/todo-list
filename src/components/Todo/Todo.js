15853a3a3ef8a5ad7d7c695ef3b7601be8fbeb92
Jace Inglis <62110248+JaceInglis@users.noreply.github.com>
2023-03-12 20:33:44 -0700

    Import edit button

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Todo.css';
+import { CiEdit } from 'react-icons/ci'
 
 import { CiCircleRemove } from 'react-icons/ci';
 
@@ -8,6 +9,7 @@ function Todo({ tasks, removeTask, editTask }) {
   return tasks?.map((task) => (
     <div className='todo-wrapper'>
       <div className='todo' key={task.id}>{task.task}</div>
+        <CiEdit className='edit-button' />
         <CiCircleRemove className='remove-button' onClick={() => removeTask(task.id)}/>
     </div>
   ))
